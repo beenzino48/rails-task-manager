f5aac239f33f3eec09ed27bc670ec4a0c81fb635
hamish liu <hamish.mc.liu@gmail.com>
2022-07-02 16:26:52 +0900

    finished

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,3 @@
 # This is class which holds my task stuff
 class Task < ApplicationRecord
-  create_table :tasks do |t|
-    t.string :title
-    t.text :details
-    t.boolean = false
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # add a route to list all tasks
-  get 'tasks', to: 'tasks#index', as: 'tasks'
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new', as: 'new_task'
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  patch 'tasks/:id', to: 'tasks#update'
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
